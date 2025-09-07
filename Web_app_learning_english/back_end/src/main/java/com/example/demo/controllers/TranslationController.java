14dfd0b8263f88cb5ae80d8ce419ffd95023e286
Danh Nguyễn Nhựt An <167954756+anoiana@users.noreply.github.com>
2025-09-07 23:02:44 +0700

    a

--- Web_app_learning_english/back_end/src/main/java/com/example/demo/controllers/TranslationController.java
+++ Web_app_learning_english/back_end/src/main/java/com/example/demo/controllers/TranslationController.java
@@ -6,7 +6,7 @@ import com.example.demo.services.TranslationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-//ádasfsdfsd
+//ádasfsdfsdádas
 @RestController
 @RequestMapping("/api/translate")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
